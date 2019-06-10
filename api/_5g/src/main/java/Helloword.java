d40ecf162b9d0c2ddd0a498ea7e231da7eba5114
Brand <490241513@qq.com>
2019-06-10 11:22:28 +0800

    测试  helloword

---
+++ api/_5g/src/main/java/Helloword.java
@@ -0,0 +1,20 @@
+/**
+ * @author admin
+ * @date 2019-06-10 11:16
+ */
+
+/**
+ * @author: Brand
+ * @Date: 2019/6/10 11:16
+ * @Description:
+ */
+
+
+public class Helloword {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello word");
+    }
+
+}
